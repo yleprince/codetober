9d377ffb32c16f7e83c32e86ed5be74ea975d514
Yrieix Leprince <yrieix.leprince@gmail.com>
2022-10-04 00:48:09 +0200

    Playing with perlin noise

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
+
 function setup() {
-    var canvas = createCanvas(600, 400);
-    canvas.parent('canvasForP5');
+    width = window.innerWidth
+    height = window.innerHeight
+
+    offset = 0
+    offset_y = 100000
+
+    seeds = Array.from({length: 50}, () => ({x:Math.random(), y:Math.random(), z: offset_y * Math.random()}));
+
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent("canvasForP5")
+
+    noStroke();
+    fill(73, 90);
 }
 
 function draw() {
-    if (mouseIsPressed){
-        console.log("click")
-        noStroke();
-
-        fill(93, 93, 93, 80);
-        ellipse(mouseX, mouseY, 20, 20);
-    } 
+    background(201, 10)
     
+    offset += 0.005
+    offset_y += 0.005
+    seeds.map(s => ellipse(windowWidth*(s.x + noise(s.z + offset))/2, windowHeight*(s.y + noise(s.z+offset_y))/2, 10, 10))
 }
